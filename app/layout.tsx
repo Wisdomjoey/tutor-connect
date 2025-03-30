5516445f849dd62cdc82538eb8b478d6cd25bc84
Wisdomjoey <joeboywizz@gmail.com>
2025-03-30 17:14:32 +0100

    update: css styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Navigation } from "@/components/navigation";
 import { Providers } from "./providers";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
